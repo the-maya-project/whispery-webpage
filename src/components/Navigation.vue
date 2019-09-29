3b50035a00b28b7d7ada3a99717b7b06ea2071f9
Koh Wei Xin <e0201350@u.nus.edu>
2019-09-30 00:27:24 +0800

    Change to Whispery text logo for navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,19 @@
 <template>
   <b-navbar>
     <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+      <b-navbar-item tag="router-link" transparent :to="{ path: '/' }">
         <img
-          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma"
+          src="../assets/whispery.png"
+          alt="Speak your mind, find like minds"
         >
       </b-navbar-item>
     </template>
-    <template slot="start">
-      <b-navbar-item href="#">
+    <!-- <template slot="start">
+      <b-navbar-item href="/">
         Home
       </b-navbar-item>
-      <b-navbar-item href="#">
-        Documentation
-      </b-navbar-item>
-      <b-navbar-dropdown label="Info">
-        <b-navbar-item href="#">
-          About
-        </b-navbar-item>
-        <b-navbar-item href="#">
-          Contact
-        </b-navbar-item>
       </b-navbar-dropdown>
-    </template>
+    </template> -->
 
     <template slot="end">
       <b-navbar-item tag="div">
